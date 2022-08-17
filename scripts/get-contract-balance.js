5c1679a95ba0d8855e4893ac6f1d9cb12c14faf3
nam <nam.pv1805@gmail.com>
2022-08-17 14:44:08 +0700

    add script

---
+++ scripts/get-contract-balance.js
@@ -0,0 +1,28 @@
+const hre = require("hardhat");
+const {ethers, } = hre;
+const {abi} = require("../artifacts/contracts/BuyMeACoffee.sol/BuyMeACoffee.json");
+require("dotenv").config();
+
+const CONTRACT_ADDRESS = process.env.DEPLOYED_ADDRESS;
+
+async function getBalance(address){
+    const balance = await ethers.provider.getBalance(address)
+    return ethers.utils.formatEther(balance);
+}
+
+async function main() {
+
+    // Get accounts
+    const [owner, ] = await ethers.getSigners();
+    // Get contract
+    const BuyMeACoffee = await ethers.getContractAt("BuyMeACoffee", CONTRACT_ADDRESS);
+    // Withdraw
+    console.log(`Contract ${BuyMeACoffee.address} balance: `, await BuyMeACoffee.getContractBalance());
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
